8f81d8a5453d78675f99c3c7683b7d1b97d7a8fc
ItsukiArakaki268 <itukiprog@gmail.com>
2025-10-10 00:01:00 +0900

    initial commit

---
+++ server/server.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+
+const express = require("express");
+const mysql = require("mysql2");
+// const cors = require("cors");
+
+const port = 3000;
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+
+const app = express();
+
+const con = mysql.createConnection({
+  host: dbHost,
+  user: dbUser,
+  password: dbPass,
+  database: dbName,
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("DB Connected");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("request to /");
+  console.log("/");
+});
+
+app.get("/post", (req, res) => {
+  res.status(200).send("request to /post");
+  console.log("/post");
+});
+
+app.get("/users_db", (req, res) => {
+  const sql = "select * from users";
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.json(result);
+  });
+});
+
+app.get("/posts_db", (req, res) => {
+  const sql = "select * from posts";
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    res.json(result);
+  });
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  const userId = req.body.user_id;
+  const content = req.body.content;
+
+  const sql = `INSERT INTO posts(user_id, content) VALUES ('${userId}', '${content}')`;
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
+    console.log(result);
+    res.json(result);
+  });
+  // res.send(`投稿しました ユーザーID: ${userId} 内容: ${content}`);
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}!`);
+});
